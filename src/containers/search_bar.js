009961e2dbd022d888d243132d3915d16d7d6d7c
yonatana <yonatana@jfrog.com>
2017-05-17 18:29:03 +0300

    dd

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -2,30 +2,52 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 function mapStateToProps(state) {
-  return {
+	return {
 
-  };
+	};
 }
 
 export class SearchBar extends React.Component {
 
 
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+		this.state = {term: ''};
 
-  render() {
-    return (
-    <form className="input-group"><input/>
-    <span className="input-group-btn"> 
-    <button className="btn btn-secondary" type="submit">Submit</button>
-    </span>
-    </form>
-    );
-  }
+		this.onInputChange = this.onInputChange.bind(this);
+		this.onSubmit = this.onSubmit.bind(this);
+	}
+
+	onInputChange(event){
+
+		event.preventDefault();
+		this.setState({term:event.target.value})
+	}
+
+	onSubmit(e){
+		console.log('need to look for',this.state.term)
+		e.preventDefault();
+	}
+
+	render() {
+		return (
+			<form onSubmit={this.onSubmit} className="input-group">
+
+			<input 
+			placeholder="Get a five-day forecast in your city"
+ 			className="form-control"
+ 			value={this.state.term}
+ 			onChange={this.onInputChange}
+			/>
+			<span className="input-group-btn"> 
+			<button className="btn btn-secondary" type="submit">Submit</button>
+			</span>
+			</form>
+			);
+	}
 }
 
 export default connect(
-  mapStateToProps,
+	mapStateToProps,
 // Implement map dispatch to props
 )(SearchBar)
